f2a238c440934c0415c8d1e77f06549f57823813
oshan-navishka <oshannavishka1234@gmail.com>
2026-03-24 17:16:20 +0530

    update typewriter

--- js/app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('cvBtn').addEventListener('click', function() {
-    const btnText = this.querySelector('.btn-text');
-    this.classList.add('downloading');
-    btnText.textContent = 'Downloading...';
-    setTimeout(() => {
-        this.classList.remove('downloading');
-        btnText.textContent = 'Download CV';
-    }, 2000);
-});
