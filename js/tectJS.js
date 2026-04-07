460e475d7dc814ca9384d0245a1f9b6fb35fa20b
oshan-navishka <oshannavishka1234@gmail.com>
2026-04-07 10:22:51 +0530

    try responive mobile size navigation

--- js/tectJS.js
+++ js/tectJS.js
@@ -1,1 +1,8 @@
-lightGallery(document.getElementById('lightgallery'))
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
